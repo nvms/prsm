ddb5676b7428348f2462d16586162f05b980cdac
nvms <jon@pye.rs>
2024-08-28 09:06:58 -0400

    ensure we distribute express-session-auth.d.ts

--- packages/express-session-auth/src/index.ts
+++ packages/express-session-auth/src/index.ts
@@ -1,5 +1,5 @@
 export { middleware as default } from "./middleware.js";
-export { User, AuthStatus, AuthRole } from "./user.entity.js";
-export { UserReset } from "./user-reset.entity.js";
 export { UserConfirmation } from "./user-confirmation.entity.js";
 export { UserRemember } from "./user-remember.entity.js";
+export { UserReset } from "./user-reset.entity.js";
+export { AuthRole, AuthStatus, User } from "./user.entity.js";
