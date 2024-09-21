bba17d3141e66f7e9b0bb2ce03e686c7463c0a07
nvms <jon@pye.rs>
2024-09-21 14:56:55 -0400

    warn once

--- packages/express-session-auth/src/util.ts
+++ packages/express-session-auth/src/util.ts
@@ -10,14 +10,17 @@ const isMiddlewareUsed = (app: express.Application, name: string) =>
       layer && layer.handle && layer.handle.name === name,
   ).length;
 
+const warnedMiddlewares = new Set<string>();
+
 export const ensureRequiredMiddlewares = (app: express.Application) => {
   const requiredMiddlewares = ["cookieParser", "session"];
 
   for (const name of requiredMiddlewares) {
-    if (!isMiddlewareUsed(app, name)) {
+    if (!isMiddlewareUsed(app, name) && !warnedMiddlewares.has(name)) {
       console.warn(
         `Required middleware '${name}' not found. Please ensure it is added to your express application.`,
       );
+      warnedMiddlewares.add(name);
     }
   }
 };
